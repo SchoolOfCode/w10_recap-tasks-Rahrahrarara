daf8ccb37f94b21769d7e935d65336ba0bfdeef9
Rahrahrarara <92094786+Rahrahrarara@users.noreply.github.com>
2022-01-31 10:55:35 +0000

    auth0 working, but have to set so that users can read artiles only after loggin in

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,19 @@
 import React from "react";
+import LoginButton from "../LoginButton";
+import LogoutButton from "../LogoutButton";
+import Profile from "../Profile";
 import css from "./header.module.css";
 
 const Header = () => {
   return (
-    <div>
-      <h1 className={css.header}>WikiPigeon</h1>
+    <div className={css.header}>
+      <h1 className={css.Logo}>WikiPigeon</h1>
+
+      <div className={css.loginsection}>
+        <Profile />
+        <LoginButton />
+        <LogoutButton />
+      </div>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 import { BrowserRouter } from "react-router-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Auth0Provider
+    domain="dev-3tlaii20.eu.auth0.com"
+    clientId="NBxyqYROwpfMUJwcxw7yK5ZZcbdtsvAV"
+    redirectUri={window.location.origin}
+  >
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    ,
+  </Auth0Provider>,
   document.getElementById("root")
 );
