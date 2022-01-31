11ed2d80782926d1d1206632f6bccb78f8ee8eb7
Rahrahrarara <92094786+Rahrahrarara@users.noreply.github.com>
2022-01-31 09:37:55 +0000

    updating error for deployment

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,7 +1,8 @@
 import ArticleDisplay from "../ArticleDisplay";
 import Home from "../Home";
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
